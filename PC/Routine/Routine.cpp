ba6ad27d88bcc7cf20f534b2f32f2f7e699c6f2e
dentych <dennis@tychsen.me>
2014-11-25 17:22:04 +0100

    Changed Routine class

--- PC/Routine/Routine.cpp
+++ PC/Routine/Routine.cpp
@@ -1,11 +1,22 @@
 #include "Routine.h"
+#include <iostream>
 
-
-Routine::Routine() {
-	// Construct!
+Routine::Routine(string name){
+	name_ = name;
 }
 
+int Routine::setDelay(int delay){
+	delay_ = delay;
+}
+void Routine::setID(char ID){
+	modtagerID.push_back(ID);
+}
 
-Routine::~Routine() {
-	// Destruct!
+int Routine::getIDSize(){
+	return modtagerID.size();
+}
+
+int Routine::getIDList(){
+	for (unsigned int i = 0; i < modtagerID.size(); i++)
+		cout << modtagerID[i] << endl;
 }
--- PC/Routine/Routine.h
+++ PC/Routine/Routine.h
@@ -1,9 +1,19 @@
 #pragma once
+using namespace std;
+#include <string>
+#include <vector>
 
 
-class Routine {
+class Routine
+{
 public:
-	Routine();
-	~Routine();
+	Routine(string name);
+	int setDelay(int delay);
+	void setID(char ID);
+	int getIDSize();
+	int getIDList();
+private:
+	string name_;
+	vector<char> modtagerID;
+	int delay_;
 };
-
