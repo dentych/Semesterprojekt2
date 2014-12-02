c997ee1f502153fc67d2ff8edc2ceecdc14e8502
dentych <dennis@tychsen.me>
2014-12-02 19:11:20 +0100

    Modtager program created

---
+++ STK Modtager/Led.h
@@ -0,0 +1,27 @@
+﻿#ifndef _LED_H
+#define _LED_H
+
+void initLEDport(unsigned char hvilkenPort); /*
+0 = PORTA
+1 = PORTB
+2 = PORTC
+3 = PORTD 
+*/
+void writeAllLEDs(unsigned char hvilkenPort, unsigned char pattern);/*
+hvilkenPort: 0=A, 1=B ...
+patteren: giver sig selv
+*/
+void turnOnLED(unsigned char hvilkenPort, unsigned char led_nr);/*
+hvilkenPort: 0=A, 1=B ...
+led_nr: tænder den valgte led
+*/
+void turnOffLED(unsigned char hvilkenPort, unsigned char led_nr);/*
+hvilkenPort: 0=A, 1=B ...
+led_nr: slukker den valgte led
+*/
+void toggleLED(unsigned char hvilkenPort, unsigned char led_nr);/*
+hvilkenPort: 0=A, 1=B ...
+<<<<<<< HEAD
+*/
+
+#endif
---
+++ STK Modtager/Modtager.h
@@ -0,0 +1,58 @@
+﻿#include "Switch.h"
+#include <avr/io.h>
+#define MAX_SWITCH_NR 7
+
+void initSwitchPort(unsigned char hvilkenPort){
+	if(hvilkenPort == 0){
+		DDRA = 0x00;
+	}
+	else if(hvilkenPort == 1){
+		DDRB = 0x00;
+	}
+	else if(hvilkenPort == 2){
+		DDRC = 0x00;
+	}
+	else if(hvilkenPort == 3){
+		DDRD = 0x00;
+	}
+}
+unsigned char switchStatus(unsigned char hvilkenPort){
+	if(hvilkenPort == 0){
+		return ~PINA;
+	}
+	else if(hvilkenPort == 1){
+		return ~PINB;
+	}
+	else if(hvilkenPort == 2){
+		return ~PINC;
+	}
+	else if(hvilkenPort == 3){
+		return ~PIND;
+	}
+}
+unsigned char switchOn(unsigned char hvilkenPort, unsigned char switch_nr){
+	if(hvilkenPort == 0){
+		if(switch_nr <= MAX_SWITCH_NR){
+			unsigned char mask = ~(PINA | ~(1<<switch_nr));
+			return mask;
+		}
+	}
+	else if(hvilkenPort == 1){
+		if(switch_nr <= MAX_SWITCH_NR){
+			unsigned char mask = ~(PINB | ~(1<<switch_nr));
+			return mask;
+		}
+	}
+	else if(hvilkenPort == 2){
+		if(switch_nr <= MAX_SWITCH_NR){
+			unsigned char mask = ~(PINC | ~(1<<switch_nr));
+			return mask;
+		}
+	}
+	else if(hvilkenPort == 3){
+		if(switch_nr <= MAX_SWITCH_NR){
+			unsigned char mask = ~(PIND | ~(1<<switch_nr));
+			return mask;
+		}
+	}
+}
